6897524c75342812da15a9e8ee926a16e5632dfb
fuchami <famichiki.yuuki@gmail.com>
2018-12-03 00:57:13 +0900

    finished

--- static/js/predict.js
+++ static/js/predict.js
@@ -1,8 +1,23 @@
+const CLASSES = {0:'zero', 1:'one', 2:'two', 3:'three', 4:'four',5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine'}
 
 /// start button event
 $("#start-button").click(function(){
+  loadModel();
   startWebcam();
 })
+
+//---------------
+// load model 
+//---------------
+let model;
+async function loadModel() {
+  console.log("model loading...");
+  $("#console").html('<li>model loading... </li>');
+  model=await tf.loadModel(`https://raw.githubusercontent.com/PonDad/manatee/master/1_sign_language_digits_classification-master/nodejs/static/sign_language_vgg16/model.json`);
+  console.log("model loaded");
+  $("#console").html('<li>trained model loaded. </li>');
+
+}
 
 //---------------
 // launch webcam
@@ -29,3 +44,71 @@ function startWebcam(){
   });
 
 }
+
+//--------------------------
+// predict button event 
+//--------------------------
+$("#predict-button").click(function(){
+  setInterval(predict, 1000/10);
+});
+
+//--------------------------
+// TensorFlow.js method 
+// predict tensor
+//--------------------------
+async function predict(){
+  let tensor = captureWebcam();
+
+  let prediction = await model.predict(tensor).data();
+  let results = Array.from(prediction).map(function(p, i){
+    return {
+      probability: p,
+      className: CLASSES[i]
+    };
+  }).sort(function(a, b){
+    return b.probability-a.probability;
+  }).slice(0.5);
+
+  $("#console").empty();
+  results.forEach(function(p){
+    $("#console").append(`<li>${p.className} : ${p.probability.toFixed(6)}</li>`);
+
+    console.log(p.className, p.probability.toFixed(6))
+  });
+};
+
+//--------------------------
+// capture streaming video 
+// to a canvas object
+//--------------------------
+function captureWebcam() {
+  var canvas = document.createElement("canvas");
+  var context = canvas.getContext('2d');
+  canvas.width = video.width;
+  canvas.height = video.height;
+
+  context.drawImage(video, 0, 0, video.width, video.height);
+  tensor_image = preprocessImage(canvas);
+
+  return tensor_image;
+}
+
+//--------------------------
+// TensorFlow.js method 
+// image to tensor
+//--------------------------
+
+function preprocessImage(image) {
+  let tensor = tf.fromPixels(image).resizeNearestNeighbor([100,100]).toFloat();
+  let offset = tf.scalar(255);
+
+  return tensor.div(offset).expandDims();
+}
+
+
+//--------------------------
+// clear button event 
+//--------------------------
+$("#clear-button").click(function clear(){
+  location.reload();
+});
